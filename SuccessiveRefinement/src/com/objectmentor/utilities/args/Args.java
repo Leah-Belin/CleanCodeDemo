cc541a14c53aca723ae676a1e407427cc8a0f4ed
LeahBelin <LazrGoose@gmail.com>
2017-10-07 18:48:53 -0400

    moved string logic into the ArgumentMarshaler

--- SuccessiveRefinement/src/com/objectmentor/utilities/args/Args.java
+++ SuccessiveRefinement/src/com/objectmentor/utilities/args/Args.java
@@ -11,7 +11,7 @@ public class Args {
     private boolean valid;
     private Set<Character> unexpectedArguments = new TreeSet<Character>();
     private Map<Character, ArgumentMarshaler> booleanArgs = new HashMap<Character, ArgumentMarshaler>();
-    private Map<Character, String> stringArgs = new HashMap<Character, String>();
+    private Map<Character, ArgumentMarshaler> stringArgs = new HashMap<Character, ArgumentMarshaler>();
     private Set<Character> argsFound = new HashSet<Character>();
     private int currentArgument;
     private char errorArgument = '\0';
@@ -22,13 +22,13 @@ enum ErrorCode{
 
 private ErrorCode errorCode = ErrorCode.OK;
 
-    public Args(String schema, String[]args) throws ParseException{
+    public Args(String schema, String[]args) throws ParseException, ArgsException{
         this.schema = schema;
         this.args = args;
         valid = parse();
     }
     
-    public boolean parse() throws ParseException{
+    public boolean parse() throws ParseException, ArgsException{
         if(schema.length() == 0 && args.length == 0)
             return true;
         parseSchema();
@@ -64,7 +64,7 @@ private ErrorCode errorCode = ErrorCode.OK;
     }
     
     private void parseStringSchemaElement(char elementId){
-        stringArgs.put(elementId, " ");
+        stringArgs.put(elementId, new StringArgumentMarshaler());
     }
     
     private boolean isStringSchemaElement(String elementTail){
@@ -79,7 +79,7 @@ private ErrorCode errorCode = ErrorCode.OK;
         booleanArgs.put(elementId, new BooleanArgumentMarshaler());
     }
     
-    private boolean parseArguments(){
+    private boolean parseArguments() throws ArgsException{
         for(currentArgument = 0; currentArgument < args.length; currentArgument++){
             String arg = args[currentArgument];
             parseArgument(arg);
@@ -87,17 +87,17 @@ private ErrorCode errorCode = ErrorCode.OK;
         return true;
     }
     
-    private  void parseArgument(String arg){
+    private  void parseArgument(String arg) throws ArgsException{
         if(arg.startsWith("-"))
             parseElements(arg);
     }
     
-    private void parseElements(String arg){
+    private void parseElements(String arg) throws ArgsException{
         for(int i = 1; i < arg.length(); i++)
             parseElement(arg.charAt(i));
     }
     
-    private void parseElement(char argChar){
+    private void parseElement(char argChar) throws ArgsException{
         if (setArgument(argChar)) 
             argsFound.add(argChar);            
         else{
@@ -106,7 +106,7 @@ private ErrorCode errorCode = ErrorCode.OK;
         }                    
     }
     
-    private boolean setArgument(char argChar){
+    private boolean setArgument(char argChar) throws ArgsException{
         boolean set = true;
         if(isBoolean(argChar))
             setBooleanArg(argChar, true);
@@ -117,14 +117,15 @@ private ErrorCode errorCode = ErrorCode.OK;
         return set;
     }
     
-    private void setStringArg(char argChar, String s){
+    private void setStringArg(char argChar, String s) throws ArgsException{
         currentArgument++;
         try{
-            stringArgs.put(argChar, args[currentArgument]);
+            stringArgs.get(argChar).setString(args[currentArgument]);
         }catch(ArrayIndexOutOfBoundsException e){
             valid = false;
             errorArgument = argChar;
             errorCode = ErrorCode.MISSING_STRING;
+            throw new ArgsException();
         }
     }
     
@@ -184,11 +185,8 @@ private ErrorCode errorCode = ErrorCode.OK;
     }
     
     public String getString(char arg){
-        return blankIfNull(stringArgs.get(arg));
-    }
-    
-    private String blankIfNull(String s){
-        return s == null?"":s;
+        Args.ArgumentMarshaler am = stringArgs.get(arg);
+        return am == null ? "" : am.getString();
     }
     
     public boolean has(char arg){
@@ -201,12 +199,21 @@ private ErrorCode errorCode = ErrorCode.OK;
     
     private class ArgumentMarshaler{
         private boolean booleanValue = false;
+        private String stringValue;
         
         public void setBoolean(boolean value){
             booleanValue = value;
         }
         
         public boolean getBoolean(){return booleanValue;}
+        
+        public void setString(String s){
+            stringValue = s;
+        }
+        
+        public String getString(){
+            return stringValue == null ? "" : stringValue;
+        }
     }
     
     private class BooleanArgumentMarshaler extends ArgumentMarshaler{
